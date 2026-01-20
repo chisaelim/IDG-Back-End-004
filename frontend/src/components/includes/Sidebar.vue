18ba1eaa86ab98183a64880979d0150147abef99
chisaelim <chisae0123@gmail.com>
2026-01-20 21:05:35 +0700

    week5

--- frontend/src/components/includes/Sidebar.vue
+++ frontend/src/components/includes/Sidebar.vue
@@ -75,7 +75,13 @@
               <p>Dashboard</p>
             </router-link>
           </li>
-          <li class="nav-header">More</li>
+          <li class="nav-header">Systems</li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'backups' }" class="nav-link" active-class="active">
+              <i class="nav-icon fas fa-database"></i>
+              <p>Backups</p>
+            </router-link>
+          </li>
         </ul>
       </nav>
     </div>
