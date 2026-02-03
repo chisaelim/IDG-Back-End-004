bea5ea7f3517d68c9d3977b29dfcb959eeee5ddb
chisaelim <chisae0123@gmail.com>
2026-02-03 16:29:07 +0700

    chat photo

--- database/migrations/2026_01_20_163104_create_chats_table.php
+++ database/migrations/2026_01_20_163104_create_chats_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
             $table->string('name', 250)->nullable();
+            $table->string('photo')->nullable();
             $table->enum('type', ['personal', 'group']);
             $table->timestamps();
         });
